fc371abf86f9a1bb982e2b10035cff70df849aed
rahulthorat0956 <rahulthorat0956@gmail.com>
2023-12-17 23:27:11 +0530

    All Changes

---
+++ src/app/teammember/teammember.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-teammember',
+  standalone: true,
+  imports: [],
+  templateUrl: './teammember.component.html',
+  styleUrl: './teammember.component.css'
+})
+export class TeammemberComponent {
+  
+
+}
