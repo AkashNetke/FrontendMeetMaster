b3a14eb4936c511165bda859e516258dd5cd22a4
rahulthorat0956 <rahulthorat0956@gmail.com>
2023-12-20 16:00:41 +0530

    Rahul Changes adding home and connection between home   login and register

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,4 +20,22 @@ export class HomeComponent {
     this._router.navigate(['/registration'])
   
   }
+  gotocontact(){
+    this._router.navigate(['./contactus'])
+  }
+  gotologin(){
+    this._router.navigate(['/login'])
+  }
+  gotomember(){
+    this._router.navigate(['/teammember'])
+  }
+  gotoservice(){
+    this._router.navigate(['/service'])
+  }
+  gotoabout(){
+    this._router.navigate(['/about'])
+  }
+  gotohome(){
+    this._router.navigate(['/home'])
+  }
 }
