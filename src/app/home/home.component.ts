5f3e549825a14481c5081a74e9e6dbec3adfad6a
rahulthorat0956 <rahulthorat0956@gmail.com>
2023-12-20 23:06:44 +0530

    Your commit message here

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,24 +18,6 @@ export class HomeComponent {
   }
   gotoregistration(){
     this._router.navigate(['/registration'])
-  
-  }
-  gotocontact(){
-    this._router.navigate(['./contactus'])
-  }
-  gotologin(){
-    this._router.navigate(['/login'])
-  }
-  gotomember(){
-    this._router.navigate(['/teammember'])
-  }
-  gotoservice(){
-    this._router.navigate(['/service'])
-  }
-  gotoabout(){
-    this._router.navigate(['/about'])
-  }
-  gotohome(){
-    this._router.navigate(['/home'])
   }
 }
+
